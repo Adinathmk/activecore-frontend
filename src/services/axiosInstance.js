f8c679d4ca9b8d768fd6fa908b58989a0b975142
Adinath <adinathmkclt@gmail.com>
2026-03-11 12:08:59 +0530

    Update API_URL constant to use environment variable for axios instance

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import store from "@/app/store";
 
-const API_URL = "i";   // IMPORTANT
+const API_URL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
